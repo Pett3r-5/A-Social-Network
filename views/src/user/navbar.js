9c7e84d04b6d05b3de6ae0c9d1481a6a9780e4f4
Petter55 <e.petter@hotmail.com>
2019-01-22 23:27:30 -0200

    navbar responsiva

--- views/src/user/navbar.js
+++ views/src/user/navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/user.css';
 import { Navbar, Nav, NavbarBrand, NavLink, Form, Collapse, Input, NavItem, Button } from 'reactstrap';
-
+import $ from 'jquery'
 
 
 class Search extends Component {
@@ -16,13 +16,65 @@ class Search extends Component {
     }
 }
 
+
+function PicNav(props) {
+  return (
+    <div id='perfilUm' className='fade-in' style={{height: 20, width: 20}}>
+      <button type='button' class='btn btn-lg btn-danger' id='photoPerfil2'
+        data-toggle='popover'
+        title= {props.userLogadoNome}
+        data-content='<a href=\"http://pudim.com.br\">Editar perfil</a>'
+        style={{backgroundImage: `url(../images/user_images/${props.userLogadoImagem})`, marginTop: '40%', border: 'none'}}>
+      </button>
+    </div>
+  )
+}
+
+
 class UserNavbar extends Component {
   constructor(props){
     super(props)
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false
+      isOpen: false,
+      userLogadoNome: 'teste',
+      userLogadoImagem: 'aaa.png',
+      picNav: null
     };
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', function () {
+      if ($(document).scrollTop() > 50) {
+        document.getElementById('navbarSupportedContent').style.paddingBottom = '0%'
+        document.getElementById('navbarSupportedContent').style.paddingTop = '0%'
+        document.getElementById('logo').style.fontSize = '100%'
+        document.getElementById('logo').style.color = 'rgb(100, 150, 200)'
+        document.getElementById('navbase').style.backgroundColor = 'rgb(250, 250, 250)'
+      } else {
+        document.getElementById('navbarSupportedContent').style.paddingBottom = '2%'
+        document.getElementById('navbarSupportedContent').style.paddingTop = '2%'
+        document.getElementById('logo').style.fontSize = '300%'
+        document.getElementById('logo').style.color = 'rgb(150, 200, 250)'
+        document.getElementById('navbase').style.backgroundColor = 'rgb(228, 199, 255)'
+      }
+      if ($(document).scrollTop() > 250) {
+        // if (!$("#perfilUm").length) {
+        // }
+        //   $(function () {
+        //     var template1 = '<a href="http://pudim.com.br" class="popover-body">editar perfil</a>'
+        //   $('#photoPerfil2').popover({html: true})
+        // })
+        this.setState({picNav: <PicNav userLogadoImagem={this.state.userLogadoImagem} userLogadoNome={this.state.userLogadoNome}/>})
+
+      } else {
+        this.setState({picNav: null})
+        // $('#perfilUm').remove()
+        // if ($( "#perfilDois" ).length) {
+        //   }
+          // $('#photoPerfil2').popover('dispose')
+      }
+    })
   }
 
   toggle(){
@@ -43,6 +95,7 @@ class UserNavbar extends Component {
               <NavItem horizontal>
                 <NavLink id="settings" href="#">Settings</NavLink>
               </NavItem>
+              {this.state.picNav}
           </Nav>
           <Search />
         </Collapse>
